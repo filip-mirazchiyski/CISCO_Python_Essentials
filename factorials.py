44d822b01adc82dd0835421b79fdf79fb73b9954
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-05 21:31:22 +0300

    Add files via upload

---
+++ factorials.py
@@ -0,0 +1,17 @@
+# 0! = 1 (yes! it's true) 1! = 1 2! = 1 * 2 3! = 1 * 2 * 3 4! = 1 * 2 * 3 * 4 : : n! = 1 * 2 ** 3 * 4 * ... * n-1 * n
+
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+
+for n in range(1, 6):  # testing
+    print(n, factorial_function(n))
+
